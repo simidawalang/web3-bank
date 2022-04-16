5994dd203970a31ffbd02a86beb6b9baee12df66
simidawalang <simidawalang@gmail.com>
2022-04-16 17:37:41 +0100

    initialized react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BankForm from './components/BankForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <BankForm />
+    </div>
+  );
+}
+
+export default App;

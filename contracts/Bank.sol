aa70fc3a8851837bf58a7431b32bad0ba066f611
simidawalang <simidawalang@gmail.com>
2022-04-16 14:15:11 +0100

    added Bank contract

---
+++ contracts/Bank.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.9;
+
+contract Bank {
+    address public bankOwner;
+    string public bankName;
+    mapping(address => uint256) public customerBalance;
+
+    constructor() {
+        bankOwner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == bankOwner, "Only the bank owner can perform this action");
+        _;
+    }
+
+    function depositMoney() public payable {
+        require(msg.value != 0, "You need to deposit some amount of money!");
+        customerBalance[msg.sender] += msg.value;
+    }
+
+    function setBankName(string memory _name) external onlyOwner {
+        bankName = _name;
+    }
+
+    function withdrawMoney(address payable _to, uint256 _total) public onlyOwner {
+        require(
+            _total <= customerBalance[msg.sender],
+            "You have insuffient funds to withdraw"
+        );
+
+        customerBalance[msg.sender] -= _total;
+        _to.transfer(_total);
+    }
+
+    function getCustomerBalance() external view returns (uint256) {
+        return customerBalance[msg.sender];
+    }
+
+    function getBankBalance() public view returns (uint256) {
+        require(
+            msg.sender == bankOwner,
+            "You must be the owner of the bank to see all balances."
+        );
+        return address(this).balance;
+    }
+}
